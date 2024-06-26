a01407d050bc6db6238e8c45aa51a42913e8dbf8
Mark Sujew <mark.sujew@typefox.io>
2024-06-26 17:40:13 +0200

    Update license header

--- packages/language/test/parsing.test.ts
+++ packages/language/test/parsing.test.ts
@@ -1,3 +1,14 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
 import { beforeAll, describe, expect, test } from "vitest";
 import { EmptyFileSystem, type LangiumDocument } from "langium";
 import { expandToString as s } from "langium/generate";
