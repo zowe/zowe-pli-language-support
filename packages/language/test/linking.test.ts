5d8ad95c5d9f4a9d76f5c1c6c57cdee06ad6ada0
Didrik Munther <dmu0817@gmail.com>
2025-01-15 15:19:28 +0100

    Add basic linking tests

--- packages/language/test/linking.test.ts
+++ packages/language/test/linking.test.ts
@@ -9,28 +9,31 @@
  *
  */
 
-import { beforeAll, describe, expect, test } from "vitest";
-import { EmptyFileSystem, type LangiumDocument } from "langium";
-import { parseHelper } from "langium/test";
-import type { PliProgram } from "pl-one-language";
+import { beforeAll, describe, test } from "vitest";
+import { EmptyFileSystem} from "langium";
+import { ExpectedGoToDefinition, expectGoToDefinition } from "langium/test";
 import { createPliServices } from "pl-one-language";
 
 let services: ReturnType<typeof createPliServices>;
-let parse:    ReturnType<typeof parseHelper<PliProgram>>;
-let parseStmts: ReturnType<typeof parseHelper<PliProgram>>;
+let gotoDefinition: ReturnType<typeof expectGoToDefinition>;
 
 beforeAll(async () => {
     services = createPliServices(EmptyFileSystem);
-    parse = parseHelper<PliProgram>(services.pli);
+    const _gotoDefinition = expectGoToDefinition(services.pli);
 
     /**
      * Helper function to parse a string of PL/I statements,
      * wrapping them in a procedure to ensure they are valid
      */
-    parseStmts = (input: string) => {
-        return parse(` STARTPR: PROCEDURE OPTIONS (MAIN);
-${input}
- end STARTPR;`);
+    gotoDefinition = (expectedGoToDefinition: ExpectedGoToDefinition) => {
+        const text = ` STARTPR: PROCEDURE OPTIONS (MAIN);
+${expectedGoToDefinition.text}
+ end STARTPR;`;
+
+        return _gotoDefinition({
+            ...expectedGoToDefinition,
+            text
+        })
     }
 
     // activate the following if your linking test requires elements from a built-in library, for example
@@ -38,9 +41,38 @@ ${input}
 });
 
 describe('Linking tests', () => {
-    test('linking of greetings', async () => {
-        const doc: LangiumDocument<PliProgram> = await parseStmts(`;`);
-        expect(doc.parseResult.lexerErrors).toHaveLength(0);
-        expect(doc.parseResult.parserErrors).toHaveLength(0);
-    });
+    describe('Declarations', async () => {
+        const text = `
+ Control: procedure options(main);
+  call <|>A('ok'); // invoke the 'A' subroutine
+ end Control;
+ <|A|>: procedure (VAR1);
+ declare <|VAR1|> char(3);
+ put skip list(V<|>AR1);
+ end <|>A;`;
+
+        test('Must find declared procedure label in CALL', async () => {
+            await gotoDefinition({
+                text: text,
+                index: 0,
+                rangeIndex: 0
+            })
+        });
+
+        test('Must find declared procedure label in END', async () => {
+            await gotoDefinition({
+                text: text,
+                index: 2,
+                rangeIndex: 0
+            })
+        });
+
+        test('Must find declared variable', async () => {
+            await gotoDefinition({
+                text: text,
+                index: 1,
+                rangeIndex: 1
+            })
+        });
+    })
 });
