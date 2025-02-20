173ea7ce35caa4533cbdb977b9bf61564ce2b87d
Benjamin Friedman Wilson <benjamin.wilson@typefox.io>
2025-02-20 17:30:33 +0100

    add validation for empty program
    Signed-off-by: Benjamin Friedman Wilson <benjamin.wilson@typefox.io>

--- packages/language/src/validation/pli-validator.ts
+++ packages/language/src/validation/pli-validator.ts
@@ -10,14 +10,14 @@
  */
 
 import type { ValidationAcceptor, ValidationChecks } from "langium";
-import type { LabelReference, Pl1AstType } from "../generated/ast.js";
+import type { LabelReference, Pl1AstType, PliProgram } from "../generated/ast.js";
 import type { Pl1Services } from "../pli-module.js";
 // Remove until grammar support for dimensions work as expected
 // import { IBM1295IE_sole_bound_specified } from './messages/IBM1295IE-sole-bound-specified.js';
 import { IBM1324IE_name_occurs_more_than_once_within_exports_clause } from "./messages/IBM1324IE-name-occurs-more-than-once-within-exports-clause.js";
 import { IBM1388IE_NODESCRIPTOR_attribute_is_invalid_when_any_parameter_has_NONCONNECTED_attribute } from "./messages/IBM1388IE-NODESCRIPTOR-attribute-is-invalid-when-any-parameter-has-NONCONNECTED-attribute.js";
 import { IBM1747IS_Function_cannot_be_used_before_the_functions_descriptor_list_has_been_scanned } from "./messages/IBM1747IS-Function-cannot-be-used-before-the-functions-descriptor-list-has-been-scanned.js";
-import { Error as PLIError, Warning } from "./messages/pli-codes.js";
+import { Error as PLIError, Severe, Warning } from "./messages/pli-codes.js";
 
 /**
  * Register custom validation checks.
@@ -27,6 +27,7 @@ export function registerValidationChecks(services: Pl1Services) {
   const validator = services.validation.Pl1Validator;
   const checks: ValidationChecks<Pl1AstType> = {
     // DimensionBound: [IBM1295IE_sole_bound_specified],
+    PliProgram: [validator.checkPliProgram],
     Exports: [IBM1324IE_name_occurs_more_than_once_within_exports_clause],
     MemberCall: [
       IBM1747IS_Function_cannot_be_used_before_the_functions_descriptor_list_has_been_scanned,
@@ -43,6 +44,21 @@ export function registerValidationChecks(services: Pl1Services) {
  * Implementation of custom validations.
  */
 export class Pl1Validator {
+
+  /**
+   * Verify programs contain at least one parsed statement
+   */
+  checkPliProgram(node: PliProgram, acceptor: ValidationAcceptor): void {
+    if (node.statements.length === 0) {
+      acceptor("error", Severe.IBM1917I.message, {
+        code: Severe.IBM1917I.fullCode,
+        node,
+        property: "statements",
+      });
+    }
+  }
+
+
   /**
    * Verify label references
    */
