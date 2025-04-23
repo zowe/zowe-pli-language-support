f7048d2f0b3eb13629aff8177a1438dc196017ff
Benjamin Friedman Wilson <benjamin.wilson@typefox.io>
2025-04-23 17:22:50 +0200

    migrate test to goto-def, remove unnecessary asyncs/awaits
    Signed-off-by: Benjamin Friedman Wilson <benjamin.wilson@typefox.io>

--- packages/language/test/linking.test.ts
+++ packages/language/test/linking.test.ts
@@ -9,18 +9,8 @@
  *
  */
 
-import { describe, expect, test } from "vitest";
-import {
-  FetchStatement,
-  ProcedureStatement,
-  Statement,
-  SyntaxKind,
-} from "../src/syntax-tree/ast";
-import { collectDiagnostics } from "../src/workspace/compilation-unit";
-import {
-  expectGotoDefinition as expectGotoDefinitionRoot,
-  parse,
-} from "./utils";
+import { describe, test } from "vitest";
+import { expectGotoDefinition as expectGotoDefinitionRoot } from "./utils";
 
 /**
  * Scoping report: https://github.com/zowe/zowe-pli-language-support/issues/94
@@ -43,7 +33,7 @@ ${params.text}
   });
 }
 
-describe("Linking tests", async () => {
+describe("Linking tests", () => {
   describe("Preprocessor variables", () => {
     test("Must find preprocessor variable", () => {
       const text = `
@@ -71,8 +61,8 @@ describe("Linking tests", async () => {
     });
   });
 
-  describe("Unstructured tests", async () => {
-    describe("Nested procedure label tests", async () => {
+  describe("Unstructured tests", () => {
+    describe("Nested procedure label tests", () => {
       const text = `
  <|OUTER|>: procedure options (main); // outer0
     <|INNER|>: procedure;             // inner0
@@ -157,7 +147,7 @@ describe("Linking tests", async () => {
 
       for (const [procedure, calls] of Object.entries(links)) {
         for (const call of calls) {
-          test(`Must link call ${call} to procedure ${procedure}`, async () => {
+          test(`Must link call ${call} to procedure ${procedure}`, () => {
             expectGotoDefinition({
               text,
               index: call,
@@ -168,8 +158,8 @@ describe("Linking tests", async () => {
       }
     });
 
-    describe("Declaration tests", async () => {
-      test("Must handle scoping in prodecures", async () => {
+    describe("Declaration tests", () => {
+      test("Must handle scoping in procedures", () => {
         const text = `
  DCL <|ABC|>;
  CALL <|>ABC;
@@ -202,7 +192,7 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must ignore redeclarations", async () => {
+      test("Must ignore redeclarations", () => {
         const text = `
  DCL <|ABC|>;
  CALL <|>ABC;
@@ -222,7 +212,7 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must find declaration in SELECT/WHEN construct", async () => {
+      test("Must find declaration in SELECT/WHEN construct", () => {
         // Taken from code_samples/PLI0001.pli
         const text = `
  SELECT (123);
@@ -241,7 +231,7 @@ describe("Linking tests", async () => {
       });
     });
 
-    describe("Declarations and labels combined", async () => {
+    describe("Declarations and labels combined", () => {
       const text = `
  Control: procedure options(main);
   call <|>A('ok!'); // invoke the 'A' subroutine
@@ -251,7 +241,7 @@ describe("Linking tests", async () => {
  put skip list(V<|>AR1);
  end <|>A;`;
 
-      test("Must find declared procedure label in CALL", async () => {
+      test("Must find declared procedure label in CALL", () => {
         expectGotoDefinition({
           text,
           index: 0,
@@ -259,7 +249,7 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must find declared procedure label in END", async () => {
+      test("Must find declared procedure label in END", () => {
         expectGotoDefinition({
           text,
           index: 2,
@@ -267,7 +257,7 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must find declared variable", async () => {
+      test("Must find declared variable", () => {
         expectGotoDefinition({
           text,
           index: 1,
@@ -276,7 +266,7 @@ describe("Linking tests", async () => {
       });
     });
 
-    describe("Qualified names", async () => {
+    describe("Qualified names", () => {
       const text = `
  DCL ARRAY_ENTRY;
  DCL TWO_DIM_TABLE_ENTRY;
@@ -297,7 +287,7 @@ describe("Linking tests", async () => {
  PUT (TABLE_WITH_ARRAY.ARRAY_ENTRY(0).<|>TYPE#);
  PUT (TABLE_WITH_ARRAY.NON_ARRAY_ENTRY.<|>TYPE#);`;
 
-      test("Must find table name in table", async () => {
+      test("Must find table name in table", () => {
         expectGotoDefinition({
           text,
           index: 0,
@@ -306,7 +296,7 @@ describe("Linking tests", async () => {
       });
 
       // TODO: Fix qualified name scoping
-      test("Must find qualified name in table", async () => {
+      test("Must find qualified name in table", () => {
         expectGotoDefinition({
           text,
           index: 1,
@@ -314,7 +304,7 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must find qualified name in array", async () => {
+      test("Must find qualified name in array", () => {
         expectGotoDefinition({
           text,
           index: 2,
@@ -322,8 +312,8 @@ describe("Linking tests", async () => {
         });
       });
 
-      test("Must find qualified name in structured variable", async () => {
-        await expectGotoDefinition({
+      test("Must find qualified name in structured variable", () => {
+        expectGotoDefinition({
           text,
           index: 3,
           rangeIndex: 3,
@@ -332,32 +322,15 @@ describe("Linking tests", async () => {
     });
   });
 
-  test("fetch linking", async () => {
-    const doc = parse(
-      `
+  test("fetch linking", () => {
+    expectGotoDefinition({
+      text: `
         MAINPR: PROCEDURE OPTIONS(MAIN);
-        dcl A entry;
-        fetch A;
-        end MAINPR;
-    `,
-      { validate: true },
-    );
-    const diagnostics = collectDiagnostics(doc);
-    expect(diagnostics.length).toBe(0);
-
-    const procedureStatement = doc.ast.statements[0]
-      .value as ProcedureStatement;
-    const subStmt = procedureStatement.statements[1] as Statement;
-    expect(
-      subStmt &&
-        subStmt.value &&
-        subStmt.value.kind === SyntaxKind.FetchStatement,
-    ).toBeTruthy();
-
-    // check that the entry ref is linked
-    const fetchStmt = subStmt.value as FetchStatement | null;
-    expect(fetchStmt).toBeDefined();
-    const ref = fetchStmt!.entries[0].entry?.ref?.node;
-    expect(ref).toBeDefined();
+        dcl <|A|> entry;
+        fetch <|>A;
+        end MAINPR;`,
+      index: 0,
+      rangeIndex: 0,
+    });
   });
 });
