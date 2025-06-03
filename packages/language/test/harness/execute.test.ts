5d9823b848fa516b51bb0203e61afa57fedade69
Didrik Munther <didrik.munther@typefox.io>
2025-06-03 10:11:20 +0200

    Add running single harness test
    Signed-off-by: Didrik Munther <didrik.munther@typefox.io>

--- packages/language/test/harness/execute.test.ts
+++ packages/language/test/harness/execute.test.ts
@@ -31,8 +31,6 @@ afterAll(() => {
   setFileSystemProvider(undefined);
 });
 
-describe("Harness tests", runHarnessTests);
-
 function getTestFiles() {
   return readdirSync(testsPath, { recursive: true })
     .map((file) => file.toString())
@@ -45,19 +43,51 @@ function getTestFiles() {
  * vitest test for each test.
  */
 function runHarnessTests() {
-  const wrappers = getWrappers();
   const files = getTestFiles();
+
+  for (const filePath of files) {
+    const fullPath = path.resolve(testsPath, filePath);
+    runSingleHarnessTest(fullPath);
+  }
+}
+
+/**
+ * Run a single harness test file.
+ *
+ * @param filePath - The path to the test file.
+ */
+function runSingleHarnessTest(filePath: string) {
+  const wrappers = getWrappers();
 
   const context = {
     wrappers,
   };
 
-  for (const file of files) {
-    const testFile = parseHarnessTestFile(
-      path.resolve(testsPath, file),
-      context,
-    );
+  const testFile = parseHarnessTestFile(filePath, context);
 
-    test(`${file}`, () => runHarnessTest(testFile, vfs));
+  test(`${filePath}`, () => runHarnessTest(testFile, vfs));
+}
+
+/**
+ * Used by launch.json to run a single test file
+ */
+const { HARNESS_TEST_FILE } = process.env;
+
+/**
+ * If HARNESS_TEST_FILE is set, run the test file specified by the environment variable.
+ */
+if (!HARNESS_TEST_FILE) {
+  describe("Harness tests", runHarnessTests);
+} else {
+  if (
+    !path
+      .relative(__dirname, HARNESS_TEST_FILE)
+      .startsWith(path.relative(__dirname, testsPath))
+  ) {
+    throw new Error(
+      `HARNESS_TEST_FILE must be a path within the tests directory, got ${HARNESS_TEST_FILE}`,
+    );
   }
+
+  runSingleHarnessTest(HARNESS_TEST_FILE);
 }
