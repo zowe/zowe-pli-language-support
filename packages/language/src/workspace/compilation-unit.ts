e5ca4120ddbe3075dbf96d9b61e9ca7319368ba7
Steven Smyth <steven.smyth@typefox.io>
2025-08-14 17:42:43 +0200

    Fix uri in case sensitive fs
    Signed-off-by: Steven Smyth <steven.smyth@typefox.io>

--- packages/language/src/workspace/compilation-unit.ts
+++ packages/language/src/workspace/compilation-unit.ts
@@ -247,7 +247,6 @@ export class CompilationUnitHandler {
       unit.requestCaches.revalidateAll({ connection, unit });
     });
     textDocuments.onDidClose((event) => {
-      this.compilationUnits.delete(event.document.uri);
       connection.sendDiagnostics({
         uri: event.document.uri,
         diagnostics: [],
