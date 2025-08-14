f20e7ca943602a2a4f6f369630b047f3ee4b3015
Mark Sujew <mark.sujew@typefox.io>
2025-08-14 17:58:20 +0200

    Add caching for tokenizer + preprocessor (#330)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/src/preprocessor/instruction-cache.ts
@@ -0,0 +1,67 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { Token } from "../parser/tokens";
+import { Statement } from "../syntax-tree/ast";
+import { URI } from "../utils/uri";
+import {
+  CompilerOptions,
+  getDefaultCompilerOptions,
+} from "./compiler-options/options";
+import * as inst from "./instructions";
+import { LexingIssue } from "./pli-lexer";
+
+interface CachedInstructions {
+  text: string;
+  instructions: FileInstructionResult;
+}
+
+export interface FileInstructionResult {
+  tokens: Token[];
+  issues: LexingIssue[];
+  statements: Statement[];
+  node: inst.InstructionNode;
+}
+
+export class InstructionCache {
+  private cache = new Map<string, CachedInstructions>();
+
+  private previousCompilerOptions = getDefaultCompilerOptions();
+
+  update(compilerOptions: CompilerOptions): void {
+    if (
+      compilerOptions.or !== this.previousCompilerOptions.or ||
+      compilerOptions.not !== this.previousCompilerOptions.not ||
+      compilerOptions.pp?.ppInclude?.value !==
+        this.previousCompilerOptions.pp?.ppInclude?.value
+    ) {
+      this.cache.clear();
+    }
+    this.previousCompilerOptions = compilerOptions;
+  }
+
+  get(
+    uri: URI,
+    text: string,
+    getter: () => FileInstructionResult,
+  ): FileInstructionResult {
+    const key = uri.toString();
+    if (this.cache.has(key)) {
+      const cached = this.cache.get(key)!;
+      if (cached.text === text) {
+        return cached.instructions;
+      }
+    }
+    const instructions = getter();
+    this.cache.set(key, { text, instructions });
+    return instructions;
+  }
+}
