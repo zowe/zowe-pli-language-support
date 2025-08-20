4e066f7e165321c199eb527b111cb7346545df7d
Mark Sujew <mark.sujew@typefox.io>
2025-08-20 14:11:41 +0200

    Complete support for preprocessor arrays (#341)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/array-assignment.ts
@@ -0,0 +1,24 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../framework.ts" />
+
+//// %DECLARE A(3) CHARACTER;
+//// %A(1) = 'A';
+//// %A(2) = 'B';
+//// %A(3) = 'C';
+//// // This should assign the whole array
+//// %B = A;
+//// %DECLARE Y CHAR;
+//// %Y = B(1) || B(2) || B(3);
+//// X = Y;
+
+preprocessor.expectTokens(" X = ABC;");
