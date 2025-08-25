feebd4235c2d5530c8687d2a1df5224fe269d0c9
Mark Sujew <mark.sujew@typefox.io>
2025-08-25 17:25:54 +0200

    Support member calls on DoType3 (#355)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

--- packages/language/src/syntax-tree/ast.ts
+++ packages/language/src/syntax-tree/ast.ts
@@ -1237,7 +1237,7 @@ export function createDoStatement(): DoStatement {
 }
 export interface DoType3 extends AstNode {
   kind: SyntaxKind.DoType3;
-  variable: ReferenceItem | null;
+  variable: MemberCall | null;
   specifications: DoSpecification[];
 }
 export function createDoType3(): DoType3 {
