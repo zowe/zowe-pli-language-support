3c4045cde0eb03618d491ae1487a34d2dd565c2a
Daniel Dietrich <daniel.dietrich@typefox.io>
2025-08-26 17:54:46 +0200

    Adds Do-Type 3 support to preprocessor interpreter (#351)
    Signed-off-by: Daniel Dietrich <daniel.dietrich@typefox.io>
    Co-authored-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/do/do-type3-while-false-immediate.ts
@@ -0,0 +1,25 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+/**
+ * DO Type 3 - WHILE condition false immediately (no iterations)
+ * Tests corner case where WHILE condition is false from the start
+ */
+
+//// %DCL I FIXED;
+//// %DO I = 1 TO 1 WHILE(I < 1);
+////   I
+//// %END;
+
+preprocessor.expectTokens(`
+`);
