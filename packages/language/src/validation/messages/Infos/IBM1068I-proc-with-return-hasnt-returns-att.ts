a236cb74bfe754452ab843d2bcfd3fae64beda4f
Wagner Laranjeiras <wagner.laranjeiras@typefox.io>
2025-08-27 13:11:07 +0200

    Implement validation for IBM1068I
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

--- packages/language/src/validation/messages/Infos/IBM1068I-proc-with-return-hasnt-returns-att.ts
+++ packages/language/src/validation/messages/Infos/IBM1068I-proc-with-return-hasnt-returns-att.ts
@@ -10,28 +10,45 @@
  */
 
 import {
-  Severity,
-  tokenToRange,
-  tokenToUri,
+Severity,
+tokenToRange,
+tokenToUri,
 } from "../../../language-server/types";
 import * as AST from "../../../syntax-tree/ast";
 import { ValidationAcceptor } from "../../validator";
 import * as PLICodes from "./../pli-codes";
 
+/** 
+ * IBM1068I: If a procedure contains a RETURN statement, it should have the RETURNS attribute specified on its PROCEDURE statement.
+ * 
+ * Triggers if a procedure has a RETURN statement, but doesn't provide a RETURNS attribute.
+ * 
+*/
 export function IBM1068I_proc_with_return_hasnt_returns_att(
   node: AST.ProcedureStatement,
   acceptor: ValidationAcceptor,
 ) {
-  const token = node.procToken;
+  // Check for RETURN statements
+  const returnStmt = node.statements?.find(stmt => stmt.value?.kind === AST.SyntaxKind.ReturnStatement);
+  if (!returnStmt) return;
+  
+  // Type-narrowing in order to access token
+  if (returnStmt.value?.kind !== AST.SyntaxKind.ReturnStatement) return;
+  const typedReturn = returnStmt.value as AST.ReturnStatement;
+  
+  // Check if procedure has RETURNS attribute - return if present
+  const hasReturnsAtt = node.options?.some(att => att.kind === AST.SyntaxKind.ReturnsOption);
+  if(hasReturnsAtt) return;
+
+  // Retrieve token of RETURN statement (NOT from the ProcedureStatement)
+  const token = typedReturn.returnToken;
   if (!token) return;
 
-  const hasReturnStatement = node.statements;
-  console.log('STATEMENTS: ', hasReturnStatement)
-
+  // Build diagnostic
   const infoRange = tokenToRange(token);
   const infoUri = tokenToUri(token);
   if (!infoRange || !infoUri) return;
-
+  
   acceptor(
     Severity.I, PLICodes.Info.IBM1068I.message, {
       code: PLICodes.Info.IBM1068I.fullCode,
