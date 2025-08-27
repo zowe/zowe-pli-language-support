e60d10137773b2852448260a9d080f463b75c684
Wagner Laranjeiras <wagner.laranjeiras@typefox.io>
2025-08-27 09:04:11 +0200

    Add test file for IBM1068I
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM1068I/proc-with-return-without-returns-att.ts
@@ -0,0 +1,23 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+/**
+ * Procedure without RETURNS but with RETURN statement must trigger IBM1068I
+ */
+
+// @wrap: main
+//// a: proc;
+////   <|1:return|>(0);
+//// end;
+
+verify.expectExclusiveErrorCodesAt(1, code.Information.IBM1068I.fullCode);
