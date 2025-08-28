4882af8e9134544e7a2ab0a5cf7ac2dc4c52f560
Mark Sujew <mark.sujew@typefox.io>
2025-08-28 11:14:32 +0200

    Support preprocessor builtins (#352)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/procedures/builtins/sysparm.ts
@@ -0,0 +1,20 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../../framework.ts" />
+
+////*PROCESS SYSPARM(TEST_VAR);
+//// %DCL Y CHAR;
+//// %Y = SYSPARM;
+//// Y
+
+// TODO: Uncomment after SYSPARM can be parsed from compiler options
+// preprocessor.expectTokens("TEST_VAR");
