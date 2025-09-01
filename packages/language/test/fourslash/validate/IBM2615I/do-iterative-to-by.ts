9e3337e3d5276b2a21c223ffc457659133302c72
Wagner-Laranjeiras <wagner.laranjeiras@typefox.io>
2025-09-01 15:58:05 +0200

    Implement support for IBM2615I (#365)
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM2615I/do-iterative-to-by.ts
@@ -0,0 +1,22 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+/**
+ * DO with TO and BY – valid – no warning
+ */
+// @wrap: main
+//// <|1:DO|> I = 1 TO 10 BY 2;
+////   CALL F;
+//// END;
+
+verify.noDiagnostics(1);
