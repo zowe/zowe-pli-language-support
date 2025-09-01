3ce7387bac9fbcb32b2cb968a8424793e2cfafcb
Wagner-Laranjeiras <wagner.laranjeiras@typefox.io>
2025-09-01 15:58:05 +0200

    Implement support for IBM2615I (#365)
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM2615I/do-iterations-must-fail.ts
@@ -0,0 +1,24 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+/**
+ * DO iteration with single assignment spec should trigger IBM2615I
+ */
+
+// @wrap: main
+//// <|1:DO|>
+////   X = 3;
+////   Y = 4;
+//// END;
+
+verify.expectExclusiveErrorCodesAt(1, code.Warning.IBM2615I.fullCode);
