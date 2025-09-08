9e43fe818b8c9284739f9ca7b0495d1bd3e32559
Wagner Laranjeiras <wagner.laranjeiras@typefox.io>
2025-09-08 10:02:15 +0200

    Implement support for IBM2412I, IBM2410I, IBM2409I
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM2412I/proc-with-return-inside-if.ts
@@ -0,0 +1,26 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+/**
+ * Procedure with RETURNS attribute and RETURN statement must NOT trigger IBM2412I
+ */
+
+// @wrap: main
+//// b: <|1:proc|> returns(fixed);
+////    if 4 > 5 then
+////        return (1);
+////    else
+////        return (0);
+//// end;
+
+verify.noDiagnostics(1);
