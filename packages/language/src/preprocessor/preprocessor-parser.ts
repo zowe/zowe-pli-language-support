22ec045a1ea9e6ed98bcd1d828e4d3cd73dc80b3
Markus Rudolph <markus.rudolph@typefox.io>
2025-09-08 12:44:16 +0200

    Add preprocessor statement %NOTE (#385)
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- packages/language/src/preprocessor/preprocessor-parser.ts
+++ packages/language/src/preprocessor/preprocessor-parser.ts
@@ -165,6 +165,10 @@ function commonStatement(state: PreprocessorParserState): ast.Statement {
         break;
       case PreprocessorTokens.Semicolon.tokenTypeIdx:
         unit = nullStatement(state);
+        break;
+      case PreprocessorTokens.Note.tokenTypeIdx:
+        unit = noteStatement(state);
+        break;
     }
   } else {
     // TODO: Handle missing preprocessor statements: NOTE, REPLACE, SELECT
@@ -231,6 +235,9 @@ function commonStatement(state: PreprocessorParserState): ast.Statement {
         break;
       case PreprocessorTokens.Return.tokenTypeIdx:
         unit = returnStatement(state);
+        break;
+      case PreprocessorTokens.Note.tokenTypeIdx:
+        unit = noteStatement(state);
         break;
     }
   }
@@ -1198,6 +1205,41 @@ function memberCall(
   const memberCall = ast.createMemberCall();
   memberCall.element = parseReferenceItem(state, withDimensions);
   return memberCall;
+}
+
+function noteStatement(state: PreprocessorParserState): ast.NoteDirective {
+  const note = ast.createNoteDirective();
+  note.noteToken = state.consume(
+    note,
+    CstNodeKind.NoteDirective_PercentNOTE,
+    PreprocessorTokens.Note,
+  );
+  state.consume(
+    note,
+    CstNodeKind.NoteDirective_OpenParen,
+    PreprocessorTokens.LParen,
+  );
+  note.message = expression(state);
+  if (
+    state.tryConsume(
+      note,
+      CstNodeKind.NoteDirective_Comma,
+      PreprocessorTokens.Comma,
+    )
+  ) {
+    note.code = expression(state);
+  }
+  state.consume(
+    note,
+    CstNodeKind.NoteDirective_CloseParen,
+    PreprocessorTokens.RParen,
+  );
+  state.consume(
+    note,
+    CstNodeKind.NoteDirective_Semicolon,
+    PreprocessorTokens.Semicolon,
+  );
+  return note;
 }
 
 function declareStatement(
