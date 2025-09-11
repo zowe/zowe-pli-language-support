77dd2a2d948b4c794557493635eb981ba394d2c1
Wagner-Laranjeiras <wagner.laranjeiras@typefox.io>
2025-09-11 15:43:32 +0200

    Implement support for IBM2412I, IBM2410I, IBM2409I (#389)
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM2412I/multiple-procs-with-valid-return-without-returns-att.ts
@@ -0,0 +1,33 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+// @wrap: main
+//// MAINPR: proc options( main );
+////    b: proc returns( OPTIONAL byvalue fixed bin(31) );
+////        return(32);
+////    end b;
+////    call b();
+////    c: <|1:proc|>;
+////        return(32);
+////    end c;
+////    call c();
+////    d: proc returns( OPTIONAL byvalue fixed bin(31) );
+////        return;
+////    end d;
+////    call d();
+////    e: proc returns( OPTIONAL byvalue fixed bin(31) );
+////    end e;
+////    call e();
+//// end MAINPR;
+
+verify.expectExclusiveErrorCodesAt(1, code.Error.IBM2412I.fullCode); // Has RETURN (...), has no RETURNS -> Needs att
