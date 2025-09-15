5888315d8e1859dd9b92ed49a1d3e6d068cdcde7
Markus Rudolph <markus.rudolph@typefox.io>
2025-09-15 13:15:11 +0200

    Add tests for scan & noscan
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/procedures/answer/answer-with-noscan-output.ts
@@ -0,0 +1,37 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../../framework.ts" />
+
+//// %DCL VAL FIXED;
+//// %VAL = 100;
+//// %DEACTIVATE VAL;
+//// %MYMACRO: PROC;
+////   ANSWER ('VAR = VAL;') NOSCAN;
+//// %END;
+//// %ACTIVATE MYMACRO;
+//// ppp: PROC;
+////   DCL VAL FIXED;
+////   VAL = 200;
+////   DCL VAR FIXED;
+////   %ACTIVATE VAL;
+////   MYMACRO
+//// END;
+
+preprocessor.expectTokens(`
+    ppp: PROC;
+        DCL VAL FIXED;
+        VAL = 200;
+        DCL VAR FIXED; 
+        VAR = VAL;
+    END;
+`);
+verify.noDiagnostics();
