eb28feb92b2cdef509a5ffd5a6ca2f55b989ba40
Markus Rudolph <markus.rudolph@typefox.io>
2025-09-17 12:31:18 +0200

    Add ANSWER preprocessor statement (#404)
    * Suppress security check's false positives for SendGrid API keys
    
    ---------
    
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/procedures/answer/answer-with-output-and-skip-side-effect.ts
@@ -0,0 +1,39 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../../framework.ts" />
+
+//// %DCL I FIXED;
+//// %I = 1;
+////
+//// %EFFECT: PROC RETURNS (FIXED);
+////   I = 2;
+////   RETURN (I);
+//// %END;
+////
+//// %MYMACRO: PROC;
+////   ANSWER ('VAR = I;') SKIP (EFFECT());
+//// %END;
+////
+//// %ACTIVATE MYMACRO;
+////
+//// ppp: PROC;
+////   DCL VAR FIXED;
+////   MYMACRO
+//// END;
+
+preprocessor.expectTokens(`
+    ppp: PROC;
+        DCL VAR FIXED;
+        VAR = 2;
+    END;
+`);
+verify.noDiagnostics();
