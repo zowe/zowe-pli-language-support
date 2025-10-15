1044c8f37f9629cb5d365633fc32be4f057dbcbb
Wagner-Laranjeiras <wagner.laranjeiras@typefox.io>
2025-10-15 15:03:14 +0200

    PATCH: Quick Fix for INCLUDE (#447)
    * Implement quick fix for unresolved INCLUDE
    
    * PATCH: implement dynamic process group retrieval
    
    ---------
    
    Signed-off-by: Wagner-Laranjeiras <wagner.laranjeiras@typefox.io>

--- packages/language/src/language-server/code-actions/apply-quick-fixes.ts
+++ packages/language/src/language-server/code-actions/apply-quick-fixes.ts
@@ -26,9 +26,15 @@ import {
 export async function quickFixResolveInclude(
   diagnostic: Diagnostic,
 ): Promise<CodeAction | undefined> {
-  const unresolvedFile = diagnostic.data?.unresolvedFile;
+  const progConfig = PluginConfigurationProviderInstance.getProgramConfig(
+    URI.parse(diagnostic.data.entryUri),
+  );
+  if (!progConfig) return;
   const procGrpsConfig =
-    PluginConfigurationProviderInstance.getProcessGroupConfig("default");
+    PluginConfigurationProviderInstance.getProcessGroupConfig(
+      progConfig.pgroup,
+    );
+  const unresolvedFile = diagnostic.data?.unresolvedFile;
   if (!procGrpsConfig || !unresolvedFile) return undefined;
 
   const configExtensions = procGrpsConfig.includeExtensions;
---
+++ packages/language/test/fourslash/preprocessor/include/dynamic-retrieve-process-group.ts
@@ -0,0 +1,49 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+// Test that specifying an absolute path to a library in proc_grps.json works correctly
+
+/// <reference path="../../framework.ts" />
+
+// @filename: .pliplugin/pgm_conf.json
+//// {
+////   "pgms": [
+////     {
+////       "program": "/progs/*.pli",
+////       "pgroup": "ipa"
+////     }
+////   ]
+//// }
+
+// @filename: .pliplugin/proc_grps.json
+//// {
+////     "pgroups": [
+////         {
+////             "name": "ipa",
+////             "libs": [
+////                 "/tmp/cpy"
+////             ],
+////             "include-extensions": [
+////                 ".pli"
+////             ]
+////         }
+////     ]
+//// }
+
+// @filename: /tmp/cpy/lib.pli
+//// DECLARE LIB_VAR FIXED;
+
+// @filename: /progs/main.pli
+//// %INCLUDE "lib.pli";
+
+preprocessor.expectTokens(`
+  DECLARE LIB_VAR FIXED;
+`);
