268044a50eea0c88b5cfb4011ff297a93b888f52
Markus Rudolph <markus.rudolph@typefox.io>
2025-11-19 14:54:54 +0100

    Add some documentation
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- packages/language/src/typesystem/computed-attributes.ts
+++ packages/language/src/typesystem/computed-attributes.ts
@@ -2,11 +2,12 @@ import { Bound, DimensionBound } from "./descriptions";
 import * as ast from "../syntax-tree/ast";
 import { evaluateExpression } from "./evaluate";
 
+/** @see https://www.ibm.com/docs/en/epfz/6.1.0?topic=arrays-dimension-attribute */
 export function computeDimensions(dimension: ast.Dimensions): Array<DimensionBound> {
     const dims = dimension.dimensions;
     const result: Array<DimensionBound> = [];
     for(const dim of dims) {
-        function computeExpression(bound: ast.Bound | undefined | null, defaultValue: number): Bound {
+        function computeExpression(bound: ast.Bound | undefined | null, defaultValue: number|undefined): Bound {
             const expr = bound?.expression;
             if (!expr) {
                 return {
@@ -38,8 +39,10 @@ export function computeDimensions(dimension: ast.Dimensions): Array<DimensionBou
             }
         }
         result.push({
+            //If only the upper bound is given, the lower bound defaults to 1.
             lowerBound: computeExpression(dim.lower, 1),
-            upperBound: computeExpression(dim.upper, 1),
+            //No default for upper bound. Assuming undefined in case of a parser error.
+            upperBound: computeExpression(dim.upper, undefined),
         });
     }
     return result;
