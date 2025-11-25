e055bf45ce8591f30cc5a7d552e0554ee40f052a
Steven Smyth <44293762+ssmifi@users.noreply.github.com>
2025-11-25 15:32:55 +0200

    Add macro pp effects (#471)
    Signed-off-by: Steven Smyth <steven.smyth@typefox.io>

---
+++ packages/language/src/validation/macro/nameprefix.ts
@@ -0,0 +1,58 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { diagnosticFromCode } from "../../language-server/types";
+import * as AST from "../../syntax-tree/ast";
+import { ValidationAcceptor } from "../validator";
+import { CompilationUnit } from "../../workspace/compilation-unit";
+import { Token } from "../../parser/tokens";
+import { PLICodes } from "../pli-codes";
+
+export function MACRO_NamePrefix(
+  node: AST.ProcedureStatement | AST.DeclaredVariable,
+  acceptor: ValidationAcceptor,
+  compilationUnit: CompilationUnit,
+) {
+  if (!compilationUnit.compilerOptions.macroOptions.namePrefix) {
+    return;
+  }
+
+  let nameTokens: Token[];
+  let name;
+
+  if (node.kind === AST.SyntaxKind.DeclaredVariable) {
+    nameTokens = node.nameToken !== null ? [node.nameToken] : [];
+  } else {
+    nameTokens =
+      node.container?.kind === AST.SyntaxKind.Statement
+        ? node.container.labels
+            ?.map((label) => label.nameToken)
+            .filter((token) => token !== null) || []
+        : [];
+  }
+
+  if (!nameTokens.length) return;
+
+  for (const nameToken of nameTokens) {
+    name = nameToken.originalImage;
+    if (
+      !nameToken ||
+      !name ||
+      name.startsWith(
+        compilationUnit.compilerOptions.macroOptions.namePrefix.character,
+      )
+    ) {
+      continue;
+    }
+
+    acceptor(diagnosticFromCode(PLICodes.Error.IBM3518I, nameToken, name));
+  }
+}
---
+++ packages/language/test/fourslash/validate/macro/deprecatenext.ts
@@ -0,0 +1,19 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+////%PROCESS PP(MACRO('DEPRECATENEXT(ENTRY(TEST))'));
+//// %<|1:TEST|>: PROC RETURNS (CHAR);
+////   RETURN (X);
+//// %END;
+
+verify.expectDiagnosticsAt(1, code.Warning.IBM3334I);
