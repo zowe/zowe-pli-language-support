9280bc94b5eef41017eaa4fc2ce22d34d39442ad
Steven Smyth <44293762+ssmifi@users.noreply.github.com>
2025-11-26 10:13:07 +0200

    Add validation for scan attribute repetition (#473)
    Signed-off-by: Steven Smyth <steven.smyth@typefox.io>

--- packages/language/src/validation/pp-validator.ts
+++ packages/language/src/validation/pp-validator.ts
@@ -4,6 +4,7 @@ import { MACRO_Deprecate } from "./macro/deprecate";
 import { MACRO_NamePrefix } from "./macro/nameprefix";
 import { MACRO_Case } from "./macro/case";
 import { ValidationChecks } from "./validator";
+import { IBM1352IE_declared_item_pp_scan_repetition } from "./compiler/IBM1352IE-declare-item-scan-repetition";
 
 export function registerPreprocessorValidationChecks(): ValidationChecks {
   return {
@@ -11,6 +12,7 @@ export function registerPreprocessorValidationChecks(): ValidationChecks {
       IBM3323I_IBM3324I_check_argument_count,
       IBM3970IS_IBM3971IS_check_pp_call_procedure,
     ],
+    DeclaredItem: [IBM1352IE_declared_item_pp_scan_repetition],
     DeclaredVariable: [MACRO_NamePrefix],
     Program: [MACRO_Case],
     ProcedureStatement: [MACRO_Deprecate, MACRO_NamePrefix],
