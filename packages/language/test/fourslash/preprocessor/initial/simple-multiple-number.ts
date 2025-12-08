5f4bfe553429a6bf0a4adb88d3b45dba9c81278f
Mark Sujew <mark.sujew@typefox.io>
2025-12-08 15:29:25 +0100

    Support `INITIAL` attribute in preprocessor (#502)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/initial/simple-multiple-number.ts
@@ -0,0 +1,19 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+//// %DECLARE A FIXED INIT(1, 2, 3, 4, 5);
+//// A
+
+// It should only pick the first one
+// TODO: Consider showing a warning for multiple initial values
+preprocessor.expectTokens("1");
