22b0c200f9572d6c9cd25314426980da2394350b
Steven Smyth <44293762+ssmifi@users.noreply.github.com>
2025-12-11 15:35:13 +0100

    Add rescan asis handling (#476)
    Signed-off-by: Steven Smyth <steven.smyth@typefox.io>

---
+++ packages/language/test/fourslash/preprocessor/rescan-asis.ts
@@ -0,0 +1,39 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../framework.ts" />
+
+// Example from pg, p. 120.
+
+////*PROCESS PP(MACRO('RESCAN(ASIS)'));
+////
+//// %dcl eins char ext;
+//// %dcl text char ext;
+////
+//// %eins = 'zwei';
+//// %text = 'EINS';
+//// display( text );
+////
+//// %text = 'eins';
+//// display( text );
+
+preprocessor.expectTokens([
+  "DISPLAY",
+  "(",
+  "zwei",
+  ")",
+  ";",
+  "DISPLAY",
+  "(",
+  "eins",
+  ")",
+  ";",
+]);
