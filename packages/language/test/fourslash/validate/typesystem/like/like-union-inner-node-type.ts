9b4536a0deac9e3d9be387c0609304a7855f6637
Markus Rudolph <markus.rudolph@typefox.io>
2026-01-08 16:30:22 +0100

    [Typesystem] Implement TYPE and LIKE attributes (#523)

---
+++ packages/language/test/fourslash/validate/typesystem/like/like-union-inner-node-type.ts
@@ -0,0 +1,35 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../../framework.ts" />
+
+// @wrap: main
+//// declare 1 person,
+////           2 address union,
+////             3 mailbox fixed(5),
+////             3 street char(100);
+//// declare <|same|> like person.address;
+
+verify.noDiagnostics();
+types.expectTypeAt("same", {
+  type: types.dataTypes.Union,
+  members: {
+    MAILBOX: {
+      type: types.dataTypes.Arithmetic,
+      scale: types.scales.Fixed,
+      precision: { totalDigitsCount: 5 },
+    },
+    STREET: {
+      type: types.dataTypes.String,
+      length: 100,
+    },
+  },
+});
