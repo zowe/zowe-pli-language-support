f130949ec6ce7925fae8d61debac461fbad86ea7
Steven Smyth <steven.smyth@typefox.io>
2026-01-16 15:12:55 +0100

    Implement deprecate validation
    Signed-off-by: Steven Smyth <steven.smyth@typefox.io>

---
+++ packages/language/test/fourslash/validate/IBM2454I-IBM2647I-deprecate-statements/deprecatenext-rewrite.ts
@@ -0,0 +1,21 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+////*PROCESS DEPRECATENEXT(STMT(REWRITE));
+//// TEST: PROC OPTIONS(MAIN);
+////   DCL MYFILE FILE;
+////   DCL X CHAR(10);
+////   <|1:REWRITE|> FILE(MYFILE) FROM(X);
+//// END TEST;
+
+verify.expectDiagnosticsAt(1, code.Warning.IBM2647I);
