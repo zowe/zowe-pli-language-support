fb279a32f4f91b85b5c54e606aff80ee8e8b1316
Markus Rudolph <markus.rudolph@typefox.io>
2026-01-19 17:20:42 +0100

    Add test for builtin functions
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

---
+++ packages/language/test/fourslash/hover/builtin-functions.ts
@@ -0,0 +1,18 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../framework.ts" />
+
+//// DCL STR CHAR(300);
+//// STR = <|1>SUBSTR('Hello, World!', 1, 5);
+
+//TODO fix the substring builtin. signature
+hover.expectMarkdownAt(1, hover.codeBlock(`SUBSTR: PROC(VALUE) RETURNS();`));
