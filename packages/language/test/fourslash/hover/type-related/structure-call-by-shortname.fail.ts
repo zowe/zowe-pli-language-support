f2c51664ee5925354315e1d5111aabd2057ab7e6
Markus Rudolph <markus.rudolph@typefox.io>
2026-01-22 16:20:54 +0100

    Add failing test
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- packages/language/test/fourslash/hover/type-related/structure-call-by-shortname.fail.ts
+++ packages/language/test/fourslash/hover/type-related/structure-call-by-shortname.fail.ts
@@ -17,9 +17,16 @@
 
 //// DCL 1 A, 2 A2, 3 A3, 2 B, 3 B3, 2 C2, 3 C3;
 //// PUT(<|1>C3);
+//// PUT(A.<|2>C3);
 
 hover.expectMarkdownAt(
   1,
+  hover.codeBlock(`DCL 1 A,
+      2 C2,
+        3 C3;`),
+);
+hover.expectMarkdownAt(
+  2,
   hover.codeBlock(`DCL 1 A,
       2 C2,
         3 C3;`),
