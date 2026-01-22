b135a3bc5f7f86c062fcc7d227bfe0d8ddd5b8e8
Mark Sujew <mark.sujew@typefox.io>
2026-01-22 12:43:19 +0100

    Improve code completion (statements/expressions) (#543)
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

---
+++ packages/language/test/fourslash/completion/pp/procedure-start.ts
@@ -0,0 +1,34 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../../framework.ts" />
+
+//// %TEST: <|outside>PROC;
+////   <|inside>
+//// %END;
+
+// Expect PROCEDURE keyword at the start of a procedure definition
+completion.expectAt("outside", {
+  includes: ["PROCEDURE"],
+});
+completion.expectAt("inside", {
+  includes: [
+    // Ensure that the labels don't use percent signs
+    "CALL",
+    "RETURN",
+    // Include all other statement-starting preprocessor keywords
+    ...constants.CompletionKeywords.StatementStartPreprocessorInProcedure.values().map(
+      (keyword) => keyword.label,
+    ),
+  ],
+  // Procedures cannot be nested in the preprocessor
+  excludes: ["PROCEDURE"],
+});
