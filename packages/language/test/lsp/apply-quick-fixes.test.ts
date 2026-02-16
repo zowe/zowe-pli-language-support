d92da60c1e7e5d5ccbfd1359c38615068d75ef1f
Wagner Laranjeiras <wagner.laranjeiras@typefox.io>
2026-02-16 12:10:09 +0100

    add more tests
    Signed-off-by: Wagner Laranjeiras <wagner.laranjeiras@typefox.io>

--- packages/language/test/lsp/apply-quick-fixes.test.ts
+++ packages/language/test/lsp/apply-quick-fixes.test.ts
@@ -164,6 +164,50 @@ describe("quickFixCreateConfig", () => {
     expect(result!.command!.arguments![0]).toBe("nested/foo.pli");
     expect(result!.command!.command).toBe(Commands.CREATE_CONFIG);
   });
+
+  test("returns valid CodeAction when absolute UNIX entryUri provided", async () => {
+    const diagnostic = {
+      data: { entryUri: "/Users/mockUser/mockFolder/foo.pli" },
+    } as Diagnostic;
+    const result = await applyQuickFixes.quickFixCreateConfig(diagnostic);
+
+    expect(result).toBeDefined();
+    expect(result!.title).toContain("Create a plugin configuration ");
+    expect(result!.kind).toBe("quickfix");
+    expect(result!.command!.arguments![0]).toBe(
+      "/Users/mockUser/mockFolder/foo.pli",
+    );
+    expect(result!.command!.command).toBe(Commands.CREATE_CONFIG);
+  });
+
+  test("returns valid CodeAction when absolute Windows entryUri provided", async () => {
+    const diagnostic = {
+      data: { entryUri: "C:\\Users\\mockUser\\mockFolder\\foo.pli" },
+    } as Diagnostic;
+    const result = await applyQuickFixes.quickFixCreateConfig(diagnostic);
+
+    expect(result).toBeDefined();
+    expect(result!.title).toContain("Create a plugin configuration ");
+    expect(result!.kind).toBe("quickfix");
+    expect(result!.command!.arguments![0]).toBe(
+      "C:/Users/mockUser/mockFolder/foo.pli",
+    );
+    expect(result!.command!.command).toBe(Commands.CREATE_CONFIG);
+  });
+  test("returns valid CodeAction when absolute Windows entryUri provided already normalized", async () => {
+    const diagnostic = {
+      data: { entryUri: "C:/Users/mockUser/mockFolder/foo.pli" },
+    } as Diagnostic;
+    const result = await applyQuickFixes.quickFixCreateConfig(diagnostic);
+
+    expect(result).toBeDefined();
+    expect(result!.title).toContain("Create a plugin configuration ");
+    expect(result!.kind).toBe("quickfix");
+    expect(result!.command!.arguments![0]).toBe(
+      "C:/Users/mockUser/mockFolder/foo.pli",
+    );
+    expect(result!.command!.command).toBe(Commands.CREATE_CONFIG);
+  });
 });
 
 //
