568906ce23f3cff215f54167801b52fb269a17e1
msujew <msujew@users.noreply.github.com>
2026-03-16 16:16:45 +0000

    Remove preview for PR 638 🛬

--- pr/pr-638/assets/_.contribution-yLVugOC6.js
+++
@@ -1,1 +0,0 @@
-class t{static{this.INSTANCE=new t}get layoutInfos(){return this._layoutInfos}constructor(){this._layoutInfos=[]}registerKeyboardLayout(s){this._layoutInfos.push(s)}}export{t as K};
--- pr/pr-638/assets/workbenchThemeService.service-Dt-9_qVy.js
+++
@@ -1,1 +0,0 @@
-import{sA as e,cB as r}from"./index-BZ1r36vO.js";const o=e(r);export{o as I};
