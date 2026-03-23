5fe828a2cd99ed1eec12cb2e7cabb7f5f5731a17
Markus Rudolph <markus.rudolph@typefox.io>
2026-03-23 13:39:35 +0100

    Add quickfix for macro casing
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

---
+++ packages/language/test/fourslash/quick-fixes/macro-case.ts
@@ -0,0 +1,34 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+/// <reference path="../framework.ts" />
+
+////*process pp(macro("CASE(UPPER)"));
+//// dcl <|variable1|> fixed;
+//// dcl variable2 char;
+
+verify.expectCompilerOptions({
+  macroOptions: {
+    case: {
+      case: constants.CompilerOptions.Macro.Case.UPPER,
+      explicitlySet: true,
+    },
+  },
+});
+verify.expectCodeActionAt(
+  "variable1",
+  "Convert to uppercase",
+  `
+  *PROCESS PP(MACRO("CASE(UPPER)"));
+  DCL VARIABLE1 FIXED;
+  DCL VARIABLE2 CHAR;
+`,
+);
