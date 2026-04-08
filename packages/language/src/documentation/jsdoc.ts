1e906fe90eee125aaf0f9dcc10aec30ff5e138bd
Markus Rudolph <markus.rudolph@typefox.io>
2026-04-08 11:33:39 +0200

    Change license headers
    Signed-off-by: Markus Rudolph <markus.rudolph@typefox.io>

--- packages/language/src/documentation/jsdoc.ts
+++ packages/language/src/documentation/jsdoc.ts
@@ -1,8 +1,13 @@
-/******************************************************************************
- * Copyright 2023 TypeFox GmbH
- * This program and the accompanying materials are made available under the
- * terms of the MIT License, which is available in the project root.
- ******************************************************************************/
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
 
 import { Position, Range } from 'vscode-languageserver-types';
 import { escapeRegExp, Token } from '../parser/tokens';
