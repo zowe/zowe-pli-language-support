bd79d8869e76473b777a449493b60bc036855d40
Mark Sujew <mark.sujew@typefox.io>
2026-07-23 14:45:01 +0200

    Update base
    Signed-off-by: Mark Sujew <mark.sujew@typefox.io>

--- packages/playground/vite.config.base.ts
+++ packages/playground/vite.config.base.ts
@@ -7,6 +7,7 @@ import type { UserConfig } from "vite";
 
 export const buildBaseConfig: () => UserConfig = () => {
   return {
+    base: "./",
     build: {
       rolldownOptions: {
         input: {
